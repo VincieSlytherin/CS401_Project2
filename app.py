29448a8abc4b1137883dddf9b547c2d41cb27b3b
VincieSlytherin <51369339+VincieSlytherin@users.noreply.github.com>
2022-02-21 14:52:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,13 +18,11 @@ import datetime
 
 app = Flask(__name__)
 
+app.version=pickle.load(open("model_v1.pickle","rb"))
+app.time=pickle.load(open("model_v1.pickle","rb"))
 app.model=pickle.load(open("model_v1.pickle","rb"))
 app.vectorizer=pickle.load(open("vectorizer.pickle","rb"))
 app.tf_transformer=pickle.load(open("tf_transformer.pickle","rb"))
-app.filePath = "model_v1.pickle"
-app.ModifiedTime=time.localtime(os.stat(app.filePath).st_mtime) #文件访问时间 
-app.time=time.strftime("%Y-%m-%d-%H:%M:%S",app.ModifiedTime)
-
 
 @app.route("/api/american",methods=["POST"])
 
@@ -44,11 +42,6 @@ def predict_country():
         predicted = app.model.predict(text2)
         prediction=predicted.tolist()
         result=""
-#         for i in predicted:
-#             result+=str(i)+" "
 
-    #     filePath = "/home/rj133/Project2/model_v1.pickle"
-    #     ModifiedTime=time.localtime(os.stat(filePath).st_mtime) #文件访问时间 
-    #     d2=time.strftime("%Y-%m-%d-%H:%M:%S",ModifiedTime)
 
-    return jsonify({"is_american":str(predicted[0]),"version":"MultinomialNB_1","model_date":app.time})
+    return jsonify({"is_american":str(predicted[0]),"version":app.version,"model_date":app.time})
