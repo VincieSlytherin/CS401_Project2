b7dcc5744e6a7fc2c4b3b5c876c6513619e51502
VincieSlytherin <51369339+VincieSlytherin@users.noreply.github.com>
2022-02-22 08:34:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,26 +23,15 @@ with open('model_v1.pickle', 'rb') as f:
     app.version=pickle.load(f)
     app.time = pickle.load(f)#time
     app.model=pickle.load(f)#model
-    
 
-# app.version=pickle.load(open("model_v1.pickle","rb"))
-# app.time=pickle.load(open("model_v1.pickle","rb"))
-# app.model=pickle.load(open("model_v1.pickle","rb"))
 app.vectorizer=pickle.load(open("vectorizer.pickle","rb"))
 app.tf_transformer=pickle.load(open("tf_transformer.pickle","rb"))
-# app.filePath = "model_v1.pickle"
-# app.ModifiedTime=time.localtime(os.stat(app.filePath).st_mtime) #文件访问时间 
-
-
-
 @app.route("/api/american",methods=["POST"])
 
 
 def predict_country():
     
-    content = request.get_json(force=True) 
-
-       
+    content = request.get_json(force=True)      
     text=np.array([content["text"]])
 
     text1=app.vectorizer.transform(text)
