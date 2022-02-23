99cc88ca13664ab42f7d30c3c87526c48796846c
VincieSlytherin <51369339+VincieSlytherin@users.noreply.github.com>
2022-02-23 14:54:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,22 +8,19 @@ import datetime
 
 app = Flask(__name__)
 
-with open('model_v1.pickle', 'rb') as f:
-    
+with open('model_v1.pickle', 'rb') as f:    
     app.version=pickle.load(f)
     app.time = pickle.load(f)#time
     app.model=pickle.load(f)#model
 
 app.vectorizer=pickle.load(open("vectorizer.pickle","rb"))
 app.tf_transformer=pickle.load(open("tf_transformer.pickle","rb"))
+
 @app.route("/api/american",methods=["POST"])
-
-
 def predict_country():
     
     content = request.get_json(force=True)      
     text=np.array([content["text"]])
-
     text1=app.vectorizer.transform(text)
     text2=app.tf_transformer.transform(text1)
     predicted = app.model.predict(text2)
